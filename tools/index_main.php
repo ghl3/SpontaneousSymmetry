935fdd125021ec53e2394f07087e5f55e5fcfd06
ghl227 <ghl227@2c42c056-1eed-47e8-bf95-8cff45d826c5>
2011-08-27 21:42:44 +0000

    General Updates
    git-svn-id: https://subversion.assembla.com/svn/spontaneoussymmetry/trunk@7 2c42c056-1eed-47e8-bf95-8cff45d826c5

--- tools/index_main.php
+++ tools/index_main.php
@@ -1,12 +1,19 @@
-
+<?php session_start(); ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-
 
 <html>
 
   <?php
      $NUM_SLASH = substr_count($_SERVER["SCRIPT_NAME"], "/") - 1;
      $HOME_DIR = ($NUM_SLASH == 0) ? "./"  : str_repeat("../", $NUM_SLASH );
+
+     // Get the variables from the POST
+     // possibly holding the information about
+     // which middle and side scripts to load
+
+     if (isset($_POST['middleColumn'])) $middle = $_POST['middleColumn'];
+     if (isset($_POST['rightColumn']))  $right  = $_POST['rightColumn'];
+
      ?>
 
   <head>	
@@ -14,6 +21,11 @@
     <meta http-equiv="Content-Type" content="text/html;charset=utf-8" >
 
     <link rel="stylesheet" href="/styles/style.css" type="text/css" media="screen">
+
+    
+    <link href="/google-code-prettify/prettify.css" type="text/css" rel="stylesheet" />
+    <script type="text/javascript" src="/google-code-prettify/prettify.js"></script>
+
 
     <?php include "title.inc"; ?>
     
@@ -23,11 +35,16 @@
 
   </head>
 
-  <body>
+  <body onload="prettyPrint()">
+
+
+    <!-- The body script uses the following variables:
+	 $middle
+	 $right
+      -->
 
     <?php include $HOME_DIR . "../tools/body.inc"; ?>
-
+    
   </body>
-
 
 </html>
