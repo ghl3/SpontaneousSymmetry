75a5ae5fee7810e0adf4deaecefb38a2d5cd7d9e
ghl227 <ghl227@2c42c056-1eed-47e8-bf95-8cff45d826c5>
2011-09-24 21:43:24 +0000

    Added Quadratic Page
    git-svn-id: https://subversion.assembla.com/svn/spontaneoussymmetry/trunk@14 2c42c056-1eed-47e8-bf95-8cff45d826c5

---
+++ public_html/code/Quadratic/submit.php
@@ -0,0 +1,34 @@
+
+
+<?php
+
+/* Open the handle to the python process,
+   Get the values, 
+   and fill the #return field (done automatically
+   using jquery). */
+
+error_reporting(E_ALL);
+
+if( isset($_POST['quada']) ){
+  if( isset($_POST['quadb']) ){
+    if( isset($_POST['quadc']) ){
+
+      $quada = $_POST['quada'];
+      $quadb = $_POST['quadb'];
+      $quadc = $_POST['quadc'];
+
+
+      /* Add redirection so we can get stderr. */
+      $handle = popen('python Quadratic.py ' . $quada . ' ' . $quadb . ' ' . $quadc . ' 2>&1', 'r');
+      $read = fread($handle, 2096);
+      echo "Solution to the Quadratic equation:<br>";
+      // $Eqn = ' ' . $quada . 'x^2 + ' $quadb . 'x + ' . $quadc . ':<br>'; 
+      //echo  $Eqn;
+      echo $read;
+      pclose($handle);
+
+    }
+  }
+}
+   
+?>
---
+++ public_html/jquery/functions/CodeVisualization.js
@@ -0,0 +1,46 @@
+
+
+
+// Javascript Functions
+
+
+// Just a test:
+
+$(document).ready(function(){
+    $("button#W3").click(function(){
+	$("p#W3").html("W3Schools", 1000);
+    });
+});
+
+$(document).ready(function(){
+    $("button#yellow").click(function(){
+	$("p#yellow").css("background-color","yellow");
+    });
+});
+
+// Show and hide code:
+
+$(document).ready(function() {
+    $('div#showcode').hide();
+});
+
+$(document).ready(function(){
+    $("button#showcode").click(function(){
+	$("div#showcode").toggle("fast");
+    });
+});
+
+
+
+$(document).ready(function(){
+    $("button").click(function(){
+	$("div").load('test1.txt');
+    });
+});
+
+$(document).ready(function(){
+    $("button").click(function(){
+	$("#test").hide();
+    });
+});
+
