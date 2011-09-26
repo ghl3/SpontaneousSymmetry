40bba4b314a1347fa60411e07ac91ac34a955def
ghl227 <ghl227@2c42c056-1eed-47e8-bf95-8cff45d826c5>
2011-09-26 18:01:39 +0000

    Major changes to common index.php
    git-svn-id: https://subversion.assembla.com/svn/spontaneoussymmetry/trunk@21 2c42c056-1eed-47e8-bf95-8cff45d826c5

---
+++ public_html/code/Quadratic/FormValidate.js
@@ -0,0 +1,48 @@
+
+
+
+$(document).ready(function(){
+    
+    // Validate
+    $("#QuadraticForm").validate({
+	debug: false,
+	rules: {
+	    quada: {
+		required: true,
+		number: true
+	    },
+	    quadb: {
+		required: true,
+		number: true
+	    },
+	    quadc: {
+		required: true,
+		number: true
+	    }
+	},
+	messages: {
+	    quada: {
+		required: "Enter the quadratic term.",
+		number: "Input must be a number"
+	    },
+	    quadb: {
+		required: "Enter the quadratic term.",
+		number: "Input must be a number"
+	    },
+	    quadc: {
+		required: "Enter the quadratic term.",
+		number: "Input must be a number"
+	    }
+
+	},
+
+
+	submitHandler: function(form) {
+	    $.post('submit.php', $("#QuadraticForm").serialize(), function(data) {
+		    <!-- Send the html results to the #results field -->
+		    $('#results').html(data);
+	    });
+	}
+    });
+});
+
