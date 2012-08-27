e1d2ccb0a8fd201ee44877ee6d8a451883755de2
George Lewis <ghl227@nyu.edu>
2012-08-27 12:35:22 -0600

    Updated wordpress theme header

--- templates/wp-theme/header.php
+++ templates/wp-theme/header.php
@@ -73,7 +73,7 @@
 				<?php $heading_tag = ( is_home() || is_front_page() ) ? 'h1' : 'div'; ?>
 				<<?php echo $heading_tag; ?> id="site-title">
 					<span>
-						<a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+						<a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><h2>Spontaneous Symmetry</h2></a>
 					</span>
 				</<?php echo $heading_tag; ?>>
 				<div id="site-description"><?php bloginfo( 'description' ); ?></div>
