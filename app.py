1fb79bf7b07482abc67da9b318f079a90b616b2b
George Lewis <ghl227@gmail.com>
2014-07-11 22:48:30 -0700

    Clean up, pep8

--- app.py
+++ app.py
@@ -20,29 +20,37 @@ app = Flask(__name__)
 
 app.register_blueprint(Blog, url_prefix='/blog')
 
+
 @app.route('/')
 def index():
     return render_template('index.html', title="SpontaneousSymmetry")
+
 
 @app.route('/info')
 def info():
     return render_template('info.html', title="Info")
 
+
 @app.route('/work')
 def work():
     return render_template('work.html', title="Work")
+
 
 @app.route('/links')
 def links():
     return render_template('links.html', title="Links")
 
+
 @app.route('/code')
 def code():
     return render_template('code.html', title="Code")
 
+
 @app.route('/RocksPaper')
 def rockspaper():
     return render_template('rockspaper.html', title="Rocks Paper")
+
+
 @app.errorhandler(404)
 def page_not_found(e):
         return render_template('errorpage.html'), 404
@@ -52,4 +60,3 @@ if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.debug = True
     app.run(host='0.0.0.0', port=port)
-
--- blog.py
+++ blog.py
@@ -28,6 +28,10 @@ import yaml
 Blog = Blueprint('blog', __name__, template_folder='blog_templates')
 
 
+POST_DIRECTORY = 'posts'
+CACHE_POSTS = True
+
+
 class Post(object):
 
     def __init__(self, path, name, date_str):
@@ -61,21 +65,32 @@ def separate_yaml(raw):
     yaml_data = yaml.load(tokens[1])
     markdown_raw = "".join(tokens[2:])
     markdown_raw = unicode(markdown_raw, errors='ignore')
-    return (yaml_data, Markup(markdown.markdown(markdown_raw, extensions=['tables', 'codehilite', 'sane_lists', 'mathjax'])))
+    extensions = ['tables', 'codehilite', 'sane_lists', 'mathjax']
+    return (yaml_data,
+            Markup(markdown.markdown(markdown_raw, extensions=extensions)))
 
 
 def memo(func):
+    """
+    Memoize the function if the global
+    variable 'CACHE_POSTS' evaluates to true
+    """
     cache = {}
+
     @wraps(func)
     def wrap(*args):
-        if args not in cache:
-            cache[args] = func(*args)
-        return cache[args]
+        if CACHE_POSTS:
+            if args not in cache:
+                cache[args] = func(*args)
+            return cache[args]
+        else:
+            return func(*args)
+
     return wrap
 
 
 @memo
-def get_all_posts(post_folder):
+def get_posts_in_directory(post_folder):
     """
     Get a list of all post data
     from a directory by looking for
@@ -96,36 +111,15 @@ def get_all_posts(post_folder):
     return posts
 
 
-def get_ordered_posts(post_folder):
-    return sorted(get_all_posts(post_folder).values(),
+@memo
+def get_posts():
+    return get_posts_in_directory(POST_DIRECTORY)
+
+
+@memo
+def get_ordered_posts():
+    return sorted(get_posts().values(),
                   key=lambda x: x.date, reverse=True)
-
-
-def get_latest_posts(n=1):
-    return get_ordered_posts("posts")[:n]
-
-
-def get_archive(post_folder, n=None):
-    """
-    An archive is an ordered dictionary of
-    month-year to a list of posts
-    """
-
-    d = defaultdict(list)
-
-    for i, post in enumerate(get_ordered_posts(post_folder)):
-
-        if n and i >= n:
-            break
-
-        year_month = post.date.replace(day=1)
-        d[year_month].append(post)
-
-    od = OrderedDict()
-    for key in sorted(d.keys(), reverse=True):
-        od[key] = d[key]
-
-    return od
 
 
 def load_post(post):
@@ -133,35 +127,64 @@ def load_post(post):
     Load a post from disk and return a dict
     of the post's contents and it's metadata
     """
-    return get_all_posts('posts')[post]
+    return get_posts()[post]
+
+
+@memo
+def get_latest_posts(n=1):
+    return get_ordered_posts()[:n]
+
+
+@memo
+def get_archive(n=None):
+    """
+    An archive is an ordered dictionary of
+    month-year to a list of posts
+    """
+
+    d = defaultdict(list)
+
+    posts = get_ordered_posts()
+
+    if n is None:
+        n = len(posts)
+
+    for post in posts:
+        year_month = post.date.replace(day=1)
+        d[year_month].append(post)
+
+    return OrderedDict(sorted(d.items(), reverse=True))
 
 
 @Blog.route('/archive/<year>/<month>')
 def archive(year, month):
     d = datetime.date(int(year), int(month), 1)
-    archive = get_archive("posts")
+    archive = get_archive()
     try:
-        posts=archive[d]
+        posts = archive[d]
     except KeyError:
         posts = []
-    return render_template('archive.html', archive={d : posts})
+    return render_template('archive.html', archive={d: posts})
 
 
 @Blog.route('/archive')
 def archive_list():
-    archive = get_archive("posts")
+    archive = get_archive()
     return render_template('archive.html', archive=archive)
 
 
 @Blog.route('/<post>')
 def blog_post(post):
-    post_data = load_post(post)
-    archive = get_archive("posts", 20)
+    try:
+        post_data = load_post(post)
+    except KeyError:
+        abort(404)
+    archive = get_archive(20)
     return render_template('post.html', post=post_data, archive=archive)
 
 
 @Blog.route('/')
 def blog():
     post_data = get_latest_posts(1)[0]
-    archive = get_archive("posts", 20)
+    archive = get_archive(20)
     return render_template('post.html', post=post_data, archive=archive)
