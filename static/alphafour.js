5b6efba110bde402a509ae2746cb2fd46f541cd2
George Lewis <ghl227@gmail.com>
2018-12-27 17:10:41 -0500

    Add grid and add pieces

--- static/alphafour.js
+++ static/alphafour.js
@@ -24,6 +24,36 @@ console.log(BOARD);
 */
 
 
+function createPiece(color) {
+
+    /*
+      <svg height="100" width="100">
+      <circle cx="50" cy="50" r="40" stroke="black" stroke-width="3" fill="red" />
+      </svg>
+    */
+
+    var svgns = "http://www.w3.org/2000/svg";
+    
+    var svg = document.createElementNS(svgns, "svg");
+    //svg.setAttribute('style', 'border: 1px solid black');
+    svg.setAttribute('width', '80');
+    svg.setAttribute('height', '80');
+    svg.setAttributeNS("http://www.w3.org/2000/xmlns/", "xmlns:xlink", "http://www.w3.org/1999/xlink");
+
+    var circle = document.createElementNS(svgns, 'circle');
+    circle.setAttributeNS(null, 'cx', 40);
+    circle.setAttributeNS(null, 'cy', 40);
+    circle.setAttributeNS(null, 'r', 30);
+    circle.setAttributeNS(null, "fill", color);
+    circle.setAttributeNS(null, "stroke", "black");
+    circle.setAttributeNS(null, "stroke-width", "3");
+    //circle.setAttributeNS(null, 'style', 'fill: none; stroke: blue; stroke-width: 1px;' );
+    svg.appendChild(circle);
+    //    svg.appendChild
+    return svg;
+}
+
+
 function placePieceInColumn(color, col) {
 
     var row = null;
@@ -50,6 +80,7 @@ function placePieceInColumn(color, col) {
     
     // Color the piece
     var elem = document.querySelector(".grid-item.row-" + row + ".col-" + col);
+    elem.appendChild(createPiece(color));
     console.log(elem);
     elem.className += " " + colorClass;
 
@@ -69,7 +100,7 @@ function createGrid(gridId) {
 
 	    var elem = document.createElement("div");
 	    elem.className = "grid-item" + " " + "space" + " " + "row-" + i + " " + "col-" + j; ////<div class="grid-item">
-	    elem.textContent = "" + i + " " + j;
+	    //elem.textContent = "" + i + " " + j;
 	    elem.onclick = function() { console.log("Clicked on " + i + " " + j); placePieceInColumn("red", j); }; 
 	    grid.appendChild(elem);
 	}
